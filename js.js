815b0ce1d075c78fcb6ce3459a25474fc27f7418
Xi-duo <1737359096@qq.com>
2023-11-21 20:34:07 +0800

    lv1 four

--- js.js
+++
@@ -1,2 +0,0 @@
-var a = 0b111;
-console.log(a);
---
+++ lv1.js
@@ -0,0 +1,18 @@
+let r = new Object();
+    r.name = 'rsj';
+    r.age = 18;
+    console.log(r);
+
+
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+}
+const r1  = new Person('rsj',18);
+const r2 = new Person('fc',18);
+
+let connect = {
+    name: 'rsj' ,
+    age: 18
+
+}
